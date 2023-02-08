cb258c6618ae4185649821cfacb2586b6640ea9e
xerxi88 <serjicu88@gmail.com>
2023-02-08 19:43:04 +0100

    TODO #6

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,7 +4,7 @@ const Comment = ({ author, text }) => {
       {/* TODO #6
       /// Una de les dues línies següents presenta un problema evident que cal arreglar. */}
       <p className='comment-author'>{author} said:</p>
-      <p className='comment-text'>text</p>
+      <p className='comment-text'>{text}</p>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 const Header = () => {
   // TODO #10
   // Crea el JSX corresponent al component Header tenint en compte la semàntica de les etiquetes.
-  return {}
+  return {
+  }
 }
 
 export default Header
