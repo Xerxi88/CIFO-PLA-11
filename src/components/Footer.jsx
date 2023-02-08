89322132a79ab38f3b1e8ff80d5c280781d1b933
xerxi88 <serjicu88@gmail.com>
2023-02-08 17:04:16 +0100

    TODO #2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
 const Footer = () => {
   return (
     // TODO #2
-    // Podem eliminar l'etiqueta footer i simplement retornar els dos paràgrafs?
-    // Quina precaució hauríem de tenir si volguéssim fer-ho? (Pista: React.Fragment)
-    <footer>
+    // Podem eliminar l'etiqueta footer i simplement retornar els dos paràgrafs? No, JSX han de tenir un element primari obligatoriament.
+    // Quina precaució hauríem de tenir si volguéssim fer-ho? Si deixem l'etiqueta buida React ho enten com un fragment.(Pista: React.Fragment)
+    <>
       <p>Instapicsum by CIFO L'Hospitalet</p>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero veritatis necessitatibus aut aliquam at,
         exercitationem voluptas voluptates nihil doloremque deserunt, nesciunt eius expedita dolor quia pariatur fugit
         beatae? Necessitatibus, beatae.
       </p>
-    </footer>
+    </>
   )
 }
 
